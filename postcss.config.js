f2e22308d4fcbb0562060eb8be0c5db5acd13e8a
totallytiia <tlaukkan@student.hive.fi>
2024-03-02 16:55:53 +0200

    tailwind fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-};
+}
