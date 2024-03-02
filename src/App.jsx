20756c342132eca39948821299b1ff493f6de670
Besnik Konjuhaj <adm-besnik.konjuhaj@alko.fi>
2024-03-02 17:40:29 +0200

    Add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import Events from './components/events'
-import Image from './components/image'
-import Service from './components/service'
+import { useState } from "react";
+import "./App.css";
+import Events from "./components/events";
+import Image from "./components/image";
+import Service from "./components/service";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Image src="src/assets/Hero_1.jpg" alt="placeholder" customClass="w-full"></Image>
+      <Image
+        src="src/assets/Hero_1.jpg"
+        alt="placeholder"
+        customClass="w-full"
+      ></Image>
       <Service></Service>
-      <Image src="src/assets/Hero_2.jpg" alt="placeholder" customClass="w-full h-96 object-cover"></Image>
+      <Image
+        src="src/assets/Hero_2.jpg"
+        alt="placeholder"
+        customClass="w-full h-96 object-cover"
+      ></Image>
       <Events></Events>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
