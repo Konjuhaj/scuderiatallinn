e243248042b5fcf723adc50fff54c0a83048b7c8
Besnik <besnik.konjuhaj@hotmail.com>
2024-03-09 12:52:48 +0200

    Add script tag

--- src/routes/cars.jsx
+++ src/routes/cars.jsx
@@ -1,6 +1,23 @@
 import Image from "../components/image";
+import { useEffect } from "react";
 
 export default function Cars() {
+  useEffect(() => {
+    const scriptBase = document.createElement("script");
+    const scriptCall = document.createElement("script");
+
+    scriptBase.src = "//www.auto24.ee/api/auto24API.js";
+    scriptBase.type = "text/javascript";
+    // script.async = true;
+    scriptCall.innerText = `auto24API.load(${import.meta.env.AUTO24KEY});`;
+
+    document.body.appendChild(scriptBase);
+    document.body.appendChild(scriptCall);
+
+    return () => {
+      document.body.removeChild(scriptBase);
+    };
+  }, []);
   return (
     <>
       <Image
@@ -8,6 +25,7 @@ export default function Cars() {
         alt="placeholder"
         customClass="w-full lg:max-h-[calc(100vh-20vh)] object-cover"
       ></Image>
+      <div id="auto24Content"></div>
     </>
   );
 }
