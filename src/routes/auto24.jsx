6073e2111122f78b5eef34246a092ac1ee7330ba
Besnik Konjuhaj <adm-besnik.konjuhaj@alko.fi>
2024-03-18 00:03:41 +0200

    Add api call to auto24 component

--- src/routes/auto24.jsx
+++ src/routes/auto24.jsx
@@ -1,6 +1,22 @@
 import Image from "../components/image";
+import { useEffect } from "react";
 
 export default function Template() {
+  useEffect(() => {
+    const root = document.querySelector("#root");
+    const script = document.createElement("script");
+    const javascriptCode = `
+    auto24API.load("80023381ff22186911bc932eff366eab");
+`;
+
+    // Create a text node containing the JavaScript code
+    const scriptContent = document.createTextNode(javascriptCode);
+
+    // Append the text node to the script element
+    script.appendChild(scriptContent);
+    const auto24Content = document.querySelector("#auto24");
+    root.insertBefore(script, auto24Content);
+  }, []);
   const divContent = "{AUTO24CONTENT}";
   return (
     <>
