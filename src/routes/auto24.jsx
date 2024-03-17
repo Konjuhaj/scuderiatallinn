8319b9a48bbf402b0b339d186da0abf86d83477f
totallytiia <tlaukkan@student.hive.fi>
2024-03-17 22:53:42 +0200

    added correct id for the div

--- src/routes/auto24.jsx
+++ src/routes/auto24.jsx
@@ -1,15 +1,9 @@
-import Image from "../components/image";
 
 export default function Template() {
   const divContent = "{AUTO24CONTENT}";
   return (
     <>
-      <Image
-        src="src/assets/Hero_1.jpg"
-        alt="placeholder"
-        customClass="w-full lg:max-h-[calc(100vh-20vh)] object-cover"
-      ></Image>
-      <div className="auto24">{divContent}</div>
+      <div className="auto24" id="auto24Content" >{divContent}</div>
     </>
   );
 }
