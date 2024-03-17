f3480e194be88945dd9279de6d8dcfadb90cb87d
totallytiia <tlaukkan@student.hive.fi>
2024-03-17 17:09:52 +0200

    basename added / and in app base removed / for base

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="" element={<Layout />}>
           <Route index element={<Root />} />
           <Route path="cars" element={<Cars />} />
           <Route path="template">
