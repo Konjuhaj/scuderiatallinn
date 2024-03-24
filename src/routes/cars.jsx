87ab71ccc6af42cf72959a644bca950d1cd91590
Besnik Konjuhaj <adm-besnik.konjuhaj@alko.fi>
2024-03-24 17:25:39 +0200

    fix : comment out cars page callback

--- src/routes/cars.jsx
+++ src/routes/cars.jsx
@@ -1,38 +1,38 @@
 import { useEffect } from "react";
 import PreLoader from "../components/preLoader";
 
-window.auto24Callback = () => {
-  console.log("auto24Callback");
+// window.auto24Callback = () => {
+//   console.log("auto24Callback");
 
-  const cars = Array.from(document.querySelectorAll(".vehicles_list > div"));
-  const { ferraries, otherCars } = cars.reduce(
-    (acc, car) => {
-      if (car.querySelector(".-make").innerText.toLowerCase() === "ferrari") {
-        acc.ferraries.push(car);
-      } else {
-        acc.otherCars.push(car);
-      }
-      return acc;
-    },
-    { ferraries: [], otherCars: [] }
-  );
+//   const cars = Array.from(document.querySelectorAll(".vehicles_list > div"));
+//   const { ferraries, otherCars } = cars.reduce(
+//     (acc, car) => {
+//       if (car.querySelector(".-make").innerText.toLowerCase() === "ferrari") {
+//         acc.ferraries.push(car);
+//       } else {
+//         acc.otherCars.push(car);
+//       }
+//       return acc;
+//     },
+//     { ferraries: [], otherCars: [] }
+//   );
 
-  const vehiclesList = document.querySelector(".vehicles_list");
-  const sortedVehicles = document.createElement("div");
-  sortedVehicles.classList.add("vehicles_list");
-  sortedVehicles.append(...ferraries);
-  sortedVehicles.append(...otherCars);
+//   const vehiclesList = document.querySelector(".vehicles_list");
+//   const sortedVehicles = document.createElement("div");
+//   sortedVehicles.classList.add("vehicles_list");
+//   sortedVehicles.append(...ferraries);
+//   sortedVehicles.append(...otherCars);
 
-  const auto24Content = document.querySelector("#auto24Content");
-  auto24Content.replaceChild(sortedVehicles, vehiclesList);
-};
+//   const auto24Content = document.querySelector("#auto24Content");
+//   auto24Content.replaceChild(sortedVehicles, vehiclesList);
+// };
 
 export default function Template() {
   useEffect(() => {
     const root = document.querySelector("#root");
     const script = document.createElement("script");
     const javascriptCode = `
-    auto24API.setCallback(window.auto24Callback);
+    //auto24API.setCallback(window.auto24Callback);
     auto24API.load("80023381ff22186911bc932eff366eab");
 `;
 
