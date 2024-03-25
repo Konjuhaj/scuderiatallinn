7e1b84b0ccf4c28120297ede997c4a622d89a56e
Besnik Konjuhaj <adm-besnik.konjuhaj@alko.fi>
2024-03-25 19:36:07 +0200

    fix : remove domain from api load

--- src/routes/cars.jsx
+++ src/routes/cars.jsx
@@ -33,7 +33,7 @@ export default function Template() {
     const script = document.createElement("script");
     const javascriptCode = `
     //auto24API.setCallback(window.auto24Callback);
-    eng.auto24API.load("80023381ff22186911bc932eff366eab");
+    auto24API.load("80023381ff22186911bc932eff366eab");
 `;
 
     // Create a text node containing the JavaScript code
